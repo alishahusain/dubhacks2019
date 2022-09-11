0b5e7514d720d5f07f3d551f051ebfee12075917
Alisha Husain <43160608+alishahusain@users.noreply.github.com>
2022-09-11 14:03:20 -0700

    Create card.js
    created card controller

---
+++ controllers/card.js
@@ -0,0 +1,21 @@
+'use strict';
+
+let CardController = function ($scope) {
+	$scope.card = {};
+	$scope.editTitle = false;
+
+	$scope.editingDetails = false;
+	$scope.editingTitle = false;
+
+
+	$scope.$on('OpenCardDetails', function(e, card){
+		$scope.card = card;
+
+		$scope.editingDetails = false;
+		$scope.editingTitle = false;
+
+		$scope.showCardDetails = true;
+	});
+
+};
+mpkModule.controller('CardController', CardController);
